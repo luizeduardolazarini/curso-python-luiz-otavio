da612e88e407930f5e110f9a024bef604cb97325
Luiz Eduardo Lazarini <luizeduardolazarini@gmail.com>
2026-07-08 23:08:23 -0300

    feat: atualizando exercicios e aulas do curso

---
+++ secao_01_iniciante/aula_04_tipo_boolean.py
@@ -0,0 +1,13 @@
+# Tipo de dado Bool (boolean)
+# Serve para representar valores lógicos, ou seja, verdadeiro ou falso.
+print(True)  # Verdadeiro
+print(False)  # Falso
+
+print(10 == 10)  # True, pois 10 é igual a 10
+print(10 != 5)  # True, pois 10 é diferente de 5
+print(10 > 5)  # True, pois 10 é maior que 5
+print(10 < 5)  # False, pois 10 não é menor que 5
+print(10 >= 10)  # True, pois 10 é maior ou igual a 10
+print(10 <= 5)  # False, pois 10 não é menor ou igual a 5
+print(10 == 5)  # False, pois 10 não é igual a 5
+print(type(True))  # <class 'bool'>
