c4ec02e3f54c87b1961c94c935bc46ceff176da6
Luiz Eduardo Lazarini <luizeduardolazarini@gmail.com>
2026-07-08 22:46:16 -0300

    feat: atualizando exercicios e aulas do curso

---
+++ secao_01_iniciante/aula_03_tipo_int_e_float.py
@@ -0,0 +1,15 @@
+# Tipos int e float 
+# int: números inteiros, sem parte decimal
+# float: números reais, com parte decimal
+# 0 tipo int é usado para representar números inteiros, enquanto o tipo float é usado para representar números reais, que podem ter uma parte decimal.
+print(10) # int
+print(-10) # int
+print(0) # int
+
+print(10.5) # float
+print(-10.5) # float
+print(0.0) # float
+
+# A função type() pode ser usada para verificar o tipo de uma variável ou valor.
+print(type(10)) # <class 'int'>
+print(type(10.5)) # <class 'float'>
