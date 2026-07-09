18c7f4e0ad309920a3747d79c2cb17ef7ea44ded
Luiz Eduardo Lazarini <luizeduardolazarini@gmail.com>
2026-07-08 22:28:45 -0300

    feat: atualizando exercicios e aulas do curso

---
+++ secao_01_iniciante/aula_02_tipos_de_dados.py
@@ -0,0 +1,6 @@
+# Aula 02 - Tipos de Dados
+# sep significa separador, ou seja, o que vai separar os valores que estão sendo impressos. Por padrão, o separador é um espaço em branco.
+# end significa final, ou seja, o que vai ser impresso no final da linha. Por padrão, o final é uma quebra de linha.
+print(12, 34, sep=' - ', end=' - ') 
+print(56, 78, sep=' - ', end=' - ')
+print(9, 10, sep=' - ')
